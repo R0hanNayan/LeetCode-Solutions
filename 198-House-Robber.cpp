816932a734c2b0cd31ffeed1a76de0b53091570a
Rohan Nayan <96344831+R0hanNayan@users.noreply.github.com>
2024-07-27 20:30:30 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 29.43%] - LeetPush

---
+++ 198-House-Robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1, -1);
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i=2; i<=n; i++){
+            int take = nums[i-1] + dp[i-2];
+            int nTake = dp[i-1];
+            dp[i] = max(take, nTake);
+        }
+        return dp[n];
+    }
+};
