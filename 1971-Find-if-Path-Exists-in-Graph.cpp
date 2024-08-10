4250aca2fd8f6bef5e13e7570776d5b4abdc7282
Rohan Nayan <96344831+R0hanNayan@users.noreply.github.com>
2024-08-11 01:26:36 +0530

    [1971] [Time Beats: 64.59%] [Memory Beats: 70.53%] - LeetPush

---
+++ 1971-Find-if-Path-Exists-in-Graph.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool bfs(vector<int> adj[], int s, int d, queue<int> &q, vector<bool> &vis){
+        q.push(s);
+        vis[s] = true;
+
+        while(!q.empty()){
+            int k = q.size();
+            while(k--){
+                int node = q.front();
+                q.pop();
+                if(node == d) return true;
+                for(auto it : adj[node]){
+                    if(!vis[it]){
+                        q.push(it);
+                        vis[it] = true;
+                    }
+                }
+            } 
+        }
+        return false;
+    }
+
+    bool validPath(int n, vector<vector<int>>& edges, int s, int d) {
+        vector<int> adj[n];
+        vector<bool> vis(n, false);
+        queue<int> q;
+
+        for(auto it : edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+
+        return bfs(adj, s, d, q, vis);
+    }
+};
