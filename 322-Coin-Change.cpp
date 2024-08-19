013e35515bdc89d74529a18dd2ff2abda07fd059
Rohan Nayan <96344831+R0hanNayan@users.noreply.github.com>
2024-08-19 21:08:02 +0530

    [322] [Time Beats: 20.62%] [Memory Beats: 9.38%] - LeetPush

--- 322-Coin-Change.cpp
+++ 322-Coin-Change.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amt) {
-        vector<int> dp(amt+1, amt+1);
-        dp[0] = 0;
+    vector<vector<int>> dp;
+    int solve(vector<int>& coins, int amt, int n){
+        if(amt == 0)  return 0;
+        if(n == 0 || amt < 0)  return INT_MAX - 1;
+        if(dp[n][amt] != -1)  return dp[n][amt];
 
-        for(int i = 0; i <= amt; i++){
-            for(auto coin : coins){
-                if(i - coin >= 0){
-                    dp[i] = min(dp[i], 1 + dp[i-coin]);
-                }
-            }
-        }
-        return dp[amt] > amt ? -1 : dp[amt];
+        int once = solve(coins, amt, n-1);
+        int twice = 1 + solve(coins, amt - coins[n-1], n);
+        dp[n][amt] = min(once, twice);
+        return dp[n][amt];
+    }
+    int coinChange(vector<int>& coins, int amt) {
+        int n = coins.size();
+        dp.resize(n+1, vector<int>(amt+1, -1));
+        int ans = solve(coins, amt, n);
+        return (ans >= INT_MAX-1) ? -1 : ans;
     }
 };
