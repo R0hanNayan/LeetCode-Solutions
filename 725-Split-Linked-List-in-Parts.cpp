8ef525137bcdfd765f456bfda8a306dda7b54dda
Rohan Nayan <96344831+R0hanNayan@users.noreply.github.com>
2024-09-09 20:32:05 +0530

    [725] [Time Beats: 89.15%] [Memory Beats: 13.51%] - LeetPush

--- 725-Split-Linked-List-in-Parts.cpp
+++ 725-Split-Linked-List-in-Parts.cpp
@@ -10,24 +10,34 @@
  */
 class Solution {
 public:
-    vector<ListNode*> splitListToParts(ListNode* root, int k) {
-        vector<ListNode*> parts(k, nullptr);
-
-        int len = 0;
-        for (ListNode* node = root; node; node = node->next)
-            len++;
-
-        int n = len / k, r = len % k;
-        ListNode* node = root, *prev = nullptr;
-
-        for (int i = 0; node && i < k; i++, r--) {
-            parts[i] = node;
-            for (int j = 0; j < n + (r > 0); j++) {
-                prev = node;
-                node = node->next;
-            }
-            prev->next = nullptr;
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        ListNode* curr = head;
+        int L = 0;
+        while(curr) {
+            L++;
+            curr = curr->next;
         }
-        return parts;
+        
+        int eachBucketNodes = L / k;
+        int remainderNodes  = L % k; 
+        
+        vector<ListNode*> result(k, NULL);
+        curr           = head;
+        ListNode* prev = NULL;
+        
+        for(int i = 0; curr && i < k ; i++) {
+            
+            result[i] = curr;
+            
+            for(int count = 1; count <= eachBucketNodes + (remainderNodes > 0 ? 1 : 0); count++) {
+                prev = curr;
+                curr = curr->next;
+            }
+            
+            prev->next = NULL;
+            remainderNodes--;
+        }
+        
+        return result;
     }
 };
