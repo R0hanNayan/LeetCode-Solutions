bfc77cd84e82ea6b36187712155e40293f599b4c
Rohan Nayan <96344831+R0hanNayan@users.noreply.github.com>
2024-09-19 20:54:06 +0530

    Create XOR_FROM_L_TO_R.cpp

---
+++ XOR_FROM_L_TO_R.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int solve(int n){
+        if(n % 4 == 1)  return 1;
+        if(n % 4 == 2)  return n+1;
+        if(n % 4 == 3)  return 0;
+        
+        return n;
+    }
+    int findXOR(int l, int r) {
+        return solve(l-1) ^ solve(r);
+    }
+};
