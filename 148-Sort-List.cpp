159abbc2bcd7a2f1c4bc5b9c4f6f1e4eeac19082
Rohan Nayan <96344831+R0hanNayan@users.noreply.github.com>
2024-10-06 15:34:56 +0530

    [148] [Time Beats: 20.72%] [Memory Beats: 28.90%] - LeetPush

--- 148-Sort-List.cpp
+++ 148-Sort-List.cpp
@@ -10,17 +10,17 @@
  */
 class Solution {
 public:
-    ListNode* findMid(ListNode* head) {
+    ListNode* findMid(ListNode* head){
         if(!head)   return NULL;
-        ListNode* slow = head;
         ListNode* fast = head->next;
+        ListNode* slow = head;
         while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
         }
+
         return slow;
     }
-
     ListNode* merge(ListNode* l1, ListNode* l2){
         ListNode* dummy = new ListNode(-1);
         ListNode* temp = dummy;
@@ -28,20 +28,18 @@ public:
         while(l1 && l2){
             if(l1->val < l2->val){
                 temp->next = l1;
-                temp = l1;
                 l1 = l1->next;
+                temp = temp->next;
             }else{
                 temp->next = l2;
-                temp = l2;
                 l2 = l2->next;
+                temp = temp->next;
             }
         }
-
-        if(l1) temp->next = l1;
-        if(l2) temp->next = l2;
+        if(l1)   temp->next = l1;
+        if(l2)   temp->next = l2;
         return dummy->next;
     }
-
     ListNode* sortList(ListNode* head) {
         if(!head || !head->next)   return head;
 
@@ -49,10 +47,8 @@ public:
         ListNode* right = mid->next;
         mid->next = NULL;
         ListNode* left = head;
-        
         left = sortList(left);
         right = sortList(right);
-
         return merge(left, right);
     }
 };
