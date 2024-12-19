37dc01d8511632cbe508e7951948122a598f55a5
Rohan Nayan <96344831+R0hanNayan@users.noreply.github.com>
2024-12-19 14:15:29 +0530

    [769] [Time Beats: 100.00%] [Memory Beats: 35.22%] - LeetPush

---
+++ 769-Max-Chunks-To-Make-Sorted.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        int sum = 0;
+        int chunks = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+            if(sum == i * (i+1)/2){
+                chunks++;
+            }
+        }
+        return chunks;
+    }
+};
