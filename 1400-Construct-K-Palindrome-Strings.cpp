5643f96df34ddc9ce76f82ef586d9e7623ba82af
Rohan Nayan <96344831+R0hanNayan@users.noreply.github.com>
2025-01-11 20:07:26 +0530

    [1400] [Time Beats: 35.31%] [Memory Beats: 21.68%] - LeetPush

---
+++ 1400-Construct-K-Palindrome-Strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        int n = s.size();
+
+        if(n < k)   return false;
+
+        unordered_map<char, int> mp;
+
+        for(auto it : s){
+            mp[it]++;
+        }
+
+        int cnt = 0;
+        for(auto it : mp){
+            if(it.second % 2 != 0){
+                cnt++;
+            }
+        }
+
+        if(cnt > k) return false;
+        return true;
+    }
+};
